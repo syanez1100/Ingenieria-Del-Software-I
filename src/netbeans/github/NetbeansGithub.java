3ef8cdb08e549fb1a74bd674efa8a9ad22640d31
SEBASTIAN <SEBASTIAN@192.168.0.102>
2021-11-16 16:16:34 -0500

    Modificacion desarrolador1

--- src/netbeans/github/NetbeansGithub.java
+++ src/netbeans/github/NetbeansGithub.java
@@ -14,8 +14,7 @@ public class NetbeansGithub {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Prueba desde netbeans");
-        System.out.println("Prueba desde Github");
+        
         
         
         
