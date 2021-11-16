5d01eea015393bca97dfd199f11c4fd3fdd2f90c
SEBASTIAN <SEBASTIAN@192.168.0.102>
2021-11-16 16:40:32 -0500

    Version1 Parcial

--- src/netbeans/github/NetbeansGithub.java
+++ src/netbeans/github/NetbeansGithub.java
@@ -14,7 +14,6 @@ public class NetbeansGithub {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Linea Rama Desarrolador1");
         
     }
     
